4364bf4e6161c255622dc120a97c72db582ac1ab
unknown <mayurubale5335@gmail.com>
2023-04-12 14:10:47 +0530

    [mayur ubale] Modify Finding Employee Attendance

--- EmployeeWageProblemUsingOops/Program.cs
+++ EmployeeWageProblemUsingOops/Program.cs
@@ -8,8 +8,26 @@ namespace EmployeeWageProblemUsingOops
 {
     internal class Program
     {
+    
         static void Main(string[] args)
         {
+            int isPresent = 1;
+            // Welcome message
+            Console.WriteLine("Welcome to Employee wage problem");
+            // UC-1 EmpAttendance
+            Random random = new Random();
+            int empAttendance = random.Next(0, 2); // 0 or 1
+           if(empAttendance == isPresent)
+            {
+                Console.WriteLine("employee attendance is present");
+            }
+            else
+            {
+                Console.WriteLine("employee attendance is absent");
+            }
+            Console.ReadLine();
+            
+
         }
     }
 }
