4fc7cdbba8968b7f6d0ee58bd833d220c4e1101f
unknown <mayurubale5335@gmail.com>
2023-04-12 14:49:13 +0530

    [mayur ubale] Modify Finding Daily Employee Wage

--- EmployeeWageProblemUsingOops/Program.cs
+++ EmployeeWageProblemUsingOops/Program.cs
@@ -11,7 +11,11 @@ namespace EmployeeWageProblemUsingOops
     
         static void Main(string[] args)
         {
+            int empHrs = 0;
             int isPresent = 1;
+            int empWage = 0;
+            const int EMP_RATE_PER_HR = 20;
+            
             // Welcome message
             Console.WriteLine("Welcome to Employee wage problem");
             // UC-1 EmpAttendance
@@ -20,11 +24,16 @@ namespace EmployeeWageProblemUsingOops
            if(empAttendance == isPresent)
             {
                 Console.WriteLine("employee attendance is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("employee attendance is absent");
+                empHrs = 0;
             }
+            // UC-2
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee wage is:" + empWage);
             Console.ReadLine();
             
 
